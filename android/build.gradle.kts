4512344021c884950f1abcb67ed1eeb946fe4ee4
André Machado <machaddr@falanet.org>
2025-09-15 10:32:08 +0100

    Downgrade Android Gradle Plugin to 8.6.1 for compatibility and pin GeckoView version to 140.0.20250707120347

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
-    id("com.android.application") version "8.13.0" apply false
-    // Downgraded Kotlin to 2.0.20 for AGP 8.6.1 compatibility
+    id("com.android.application") version "8.6.1" apply false
     id("org.jetbrains.kotlin.android") version "2.0.20" apply false
 }
 
 // Pinned GeckoView latest release (from maven-metadata.xml <release>)
-extra["geckoviewVersion"] = "141.0.20250806102122"
+extra["geckoviewVersion"] = "140.0.20250707120347"
