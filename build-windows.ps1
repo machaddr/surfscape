aebfb151581414082f79328df3b82f1cdbe2de63
André Machado <machaddr@falanet.org>
2025-10-11 15:26:28 +0100

    Update Version to 1.1

--- build-windows.ps1
+++ build-windows.ps1
@@ -84,7 +84,7 @@ Write-Step ("Using Python command: " + ($py -join ' '))
 Clean-Windows
 
 # Determine version from setup.py (fallback to 1.0)
-$version = '1.0'
+$version = '1.1'
 try {
     $setup = Get-Content -Raw -Path "setup.py"
     if ($setup -match 'version="([0-9]+\.[0-9]+\.[0-9]+)"') { $version = $Matches[1] }
